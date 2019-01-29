258f18345ef904811eb443287610f14607cf83e2
Laura Ambrose <laura@kintohub.com>
2019-01-29 12:12:07 +0800

    added routes and pose

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
